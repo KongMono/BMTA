2ccff1ef1ae3e7449b7ddb059cd21920138bceaf
KongMono <koyngben@gmail.com>
2015-07-29 04:06:01 +0700

    update sync

---
+++ SqliteForWP8/Item/busstop.cs
@@ -0,0 +1,28 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BMTA
+{
+    public class busstop
+    {
+        [SQLite.PrimaryKey, SQLite.AutoIncrement]
+        public int id { get; set; }
+
+        public string stop_name { get; set; }
+
+        public string stop_name_en { get; set; }
+
+        public string stop_description { get; set; }
+
+        public string latitude { get; set; }
+
+        public string longitude { get; set; }
+
+        public string modify_date { get; set; }
+
+        public int status { get; set; }
+    }
+}
